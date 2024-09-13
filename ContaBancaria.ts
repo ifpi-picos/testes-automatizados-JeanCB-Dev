a35c46372bde1c5cb6f28c8b270f10cc15f615f2
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-09-13 14:58:38 -0300

    adição dos metodos da Conta Bancária

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,31 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    numeroConta = 1
+    agencia = 1010
+    private saldo = 0
+    extrato = []
+
+
+    public depositar(valor: number) {
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    public sacar(valor: number) { 
+        if (valor > 0) {
+            this.saldo -= valor
+        }
+    }
+
+    public consultarSaldo() { 
+        return this.saldo
+    }
+
+    public consultarExtrato() {
+        return this.extrato
+    }
+    private registraroperacao() { 
+        
+    }
+
+}
